e101c8ea86de7dfa0a1eaa0dc121f0ad122fef9d
Jack Eden Hirschman <jhirschm@sdfiana008.sdf.slac.stanford.edu>
2024-08-16 17:39:12 -0700

    local branch

--- test_script_s3df.sh
+++ test_script_s3df.sh
@@ -2,12 +2,25 @@
 #SBATCH --partition=ampere
 ## SBATCH --account=lcls:prjs2e21
 #SBATCH --job-name=reg
-#SBATCH --output=/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/s3df_runtime_outputs/output-%j.txt
-#SBATCH --error=/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/s3df_runtime_outputs/output-%j.txt
+#SBATCH --output=/sdf/data/lcls/ds/prj/prjs2e21/results/DCNS_LSTM_Output/s3df_runtime_outputs/output-%j.txt
+#SBATCH --error=/sdf/data/lcls/ds/prj/prjs2e21/results/DCNS_LSTM_Output/s3df_runtime_outputs/output-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=32g
-#SBATCH --time=0-24:00:00
-#SBATCH --gpus 4
+#SBATCH --time=0-1:00:00
+#SBATCH --gpus 1
+source ~/conda.sh
 
-python3 main.py --model LSTM --data_dir /fs/ddn/sdf/group/lcls/ds/scratch/s2e_scratch/Data/DCNS_NLO_LSTM_H5_Data --output_dir "/sdf/scratch/lcls/ds/prj/prjs2e21/scratch/dcns_lstm_output/test_06032024" --custom_code 1 --batch_size 200 --load_in_gpu 0
+echo starting run 1 at: `date`
+# Check which GPU is being used
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+# Run the Python script with the specified arguments
+export PYTHONUNBUFFERED=1
+export PYTHONIOENCODING=utf-8
+
+#python3 main.py --model LSTM --data_dir /fs/ddn/sdf/group/lcls/ds/scratch/s2e_scratch/Data/DCNS_NLO_LSTM_H5_Data --output_dir "/sdf/scratch/lcls/ds/prj/prjs2e21/scratch/dcns_lstm_output/test_06032024" --custom_code 1 --batch_size 200 --load_in_gpu 0
+
+python3 main.py --model LSTM --data_dir /fs/ddn/sdf/group/lcls/ds/scratch/s2e_scratch/Data/DCNS_NLO_LSTM_H5_Data --do_analysis 1 --model_param_path /fs/ddn/sdf/group/lcls/ds/scratch/s2e_scratch/Data/DCNS_NLO_LSTM_H5_Data/LSTM_120_epoch_44.pth --analysis_file 91 --analysis_example 15 --output_dir /sdf/data/lcls/ds/prj/prjs2e21/results/DCNS_LSTM_Output/
+
+#python3 main.py --model LSTM --data_dir /fs/ddn/sdf/group/lcls/ds/scratch/s2e_scratch/Data/DCNS_NLO_LSTM_H5_Data --do_prediction 1 --model_param_path /fs/ddn/sdf/group/lcls/ds/scratch/s2e_scratch/Data/DCNS_NLO_LSTM_H5_Data/LSTM_120_epoch_44.pth --output_dir /sdf/data/lcls/ds/prj/prjs2e21/results/DCNS_LSTM_Output/
+echo Finished at: `date`
